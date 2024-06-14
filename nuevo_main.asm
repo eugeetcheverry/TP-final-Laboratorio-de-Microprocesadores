84982cbef93465d85b0aa953856e9259ddc2b1dc
María Eugenia Etcheverry <114704607+eugeetcheverry@users.noreply.github.com>
2024-06-14 09:22:07 -0300

    Add files via upload

---
+++ nuevo_main.asm
@@ -0,0 +1,270 @@
+.include "m328pdef.inc"
+
+.def flag_e = r17 ;0b0000dcba
+;a = eligiendo contrincante
+;b = eligiendo numero
+;c = juego
+;d = nada
+.def flag_int = r18 ;0b00fedcba
+;a = int0
+;b = usart rx
+;c = usart tx
+;d = adc_inc
+;e = adc_dec
+;f = timer
+.def contador_tabla = r19
+.def cero = r20
+.def rleds = r21
+.def cont_dgt = r22
+
+.dseg
+
+.org SRAM_START
+
+TABLA: .byte 10
+NUM_ELEGIDO: .byte 4
+NUM_JUGANDO: .byte 4
+
+.cseg
+
+.org 0X0000
+	rjmp main
+
+;Sector de interrupciones por INT0
+.org INT0addr
+	rjmp int_push_btm
+
+;Sector de interrupciones USART
+.org URXCaddr
+	rjmp int_usart_rx
+
+.org UTXCaddr
+	rjmp int_usart_tx
+
+.org ADCCaddr
+	rjmp int_adc
+
+.org INT_VECTORS_SIZE
+
+main:
+	;Inicializo stack-pointer
+	ldi r16, low(RAMEND)
+	out spl, r16
+	ldi r16, high(RAMEND)
+
+	;inicializo los puertos 
+	ldi r16, 0b01011111
+	out ddrc, r16
+
+	ldi r16, 0b00001111
+	out ddrb, r16
+
+	
+	;Seteo las interrupciones
+	;Seteo interrupcion por: INT0
+	ldi r16,  0x02 ;por flanco descendente 
+	sts EICRA, r16
+	ldi r16, 0x01
+	sts EIMSK, r16
+
+	;Seteo interrupcion por: USART
+	;Recibo datos y envio
+	ldi r16, 0x0c
+	sts UBRR0L, r16
+	clr r16
+	sts UBRR0H, r16
+	ldi r16, 0b00100010 ;velocidad doble
+	sts USCR0A, r16
+	ldi r16, 0b00000110
+	sts USCR0C, r16
+	ldi r16, 0b11011000
+	sts USCR0B, r16  
+
+	;Seteo interrupcion por: TIMER
+	sei
+
+chequeo_etapa:
+	sbic flag_e, 0 
+	rcall eligiendo_contrincante
+	sbic flag_e, 1
+	rcall eligiendo_numero
+	sbrc flag_e, 2
+	rcall juego
+	rjmp loop_chequeo_etapa
+
+;ETAPA: ELIGIENDO CONTRINCANTE----------------------------------------------------------------------
+eligiendo_contricante:
+	sbic flag_int, 0
+	rcall push_btm_etapa1
+	sbrc flag_int, 1
+	rcall usart_rx_etapa1
+	ret
+
+usart_rx_etapa1:
+	lds r16, UDR0
+	cpi r16, 78 ;Comparo con N
+	sbrc sreg, 1
+	rcall pasar_eligiendo_numero
+	ret
+
+push_btm_etapa1:
+	ldi r16, 78
+	sts UDR0, r16
+	rcall pasar_eligiendo_numero
+	ret
+
+pasar_eligiendo_numero:
+	clr flag_int
+	ldi flag_e, 0b00000010
+	rcall led_titilando
+	;Inicializo puntero a tabla
+	ldi XL, low(TABLA)
+	ldi XH, high(TABLA)
+
+	rcall limpiar_tabla
+
+	clr cero
+	clr cont_dgt
+	ldi rleds, 0b00000001
+	;Seteo interrupcion por: ADC
+	cli ; Deshabilita las interrupciones 
+
+	ldi r16, 0b11011111;Dejo deshabilitado el trigger
+	sts ADCSRA, r16 ;Seteo tension de referencia y frecuencia de la señal ADC Clock es fosc/128
+
+	ldi r16, 0x00
+	sts ADCSRB, r16
+
+	ldi r16, 0b01100100 ;ADC4
+	sts ADMUX, r16 
+	sei
+
+	ret
+
+limpiar_tabla:
+	ldi r16, 0
+	ld X+, 0
+	inc contador_tabla
+	cpi contador_tabla, 9
+	sbrs sreg, 1
+	rjmp limpiar_tabla
+	clr contador_tabla
+	ret
+
+;ETAPA: ELIGIENDO NUMERO--------------------------------------------
+eligiendo_numero:
+	out PORTC, rleds
+	sbic flag_int, 0
+	rcall elige_numero
+	sbrc flag_e, 2
+	rcall chequeo_etapa
+	sbrc flag_int, 3 
+	rcall incremento
+	sbrc flag_int, 4
+	rcall decremento
+	ret
+
+elige_numero:
+	lsr rleds
+	mov r16, contador_tabla
+	sts NUM_ELEGIDO + cont_dgt, r16
+	inc cont_dgt
+	sbrc cont_dgt, 2
+	rcall pasar_juego
+	ret
+
+incremento:
+	inc contador_tabla
+	cpi contador_tabla, 10
+	breq 
+	ld r16, X+
+	sbrc r16, 0
+	rcall muevo_puntero_inc
+	ret
+
+muevo_puntero_inc:
+	inc contador_tabla
+	cpi contador_tabla, 10
+	breq 
+	ldi XL, low(TABLA)
+	ldi XH, high(TABLA)
+	add XL, contador_tabla
+	adc XH, cero
+	ld r16, X
+	sbrc r16, 0
+	rjmp muevo_puntero_inc
+	out PORTB, contador_tabla
+	ret
+
+decremento:
+	dec contador_tabla
+	cpi contador_tabla, 0
+	breq 
+	ld r16, -X
+	sbrc r16, 0
+	rcall muevo_puntero_dec
+	out PORTB, contador_tabla
+	ret
+
+muevo_puntero_dec:
+	dec contador_tabla
+	cpi contador_tabla, 0
+	breq 
+	ldi XL, low(TABLA)
+	ldi XH, high(TABLA)
+	sub XL, contador_tabla
+	sbc XH, cero
+	ld r16, X
+	sbrc r16, 0
+	rjmp muevo_puntero_dec
+	ret
+
+pasar_juego:
+	clr flag_int
+	rcall limpiar_tabla
+	ldi flag_e, 0b00000100
+	ldi XL, low(NUM_JUGANDO)
+	ldi XH, high(NUM_JUGANDO)
+	ldi YL, low(NUM_ELEGIDO)
+	ldi YH, high(NUM_ELEGIDO)
+	clr r16
+	sts ADCSRA, r16;Apago contador
+	ret
+
+;ETAPA: JUEGO-----------------------------------------------------------
+
+;INTERRUPCIONES---------------------------------------------------------
+int_int0:
+	sbi flag_int, 0
+	reti
+
+int_usart_rx:
+	sbi flag_int, 1
+	reti
+
+int_usart_tx:
+	clr flag_int
+	sbi flag_int, 2
+	reti
+
+int_adc:
+	clr flag_int
+	lds r16, ADCH
+	cpi r16, valor_max
+	brsh int_ad_inc
+	cpi r16, valor_min
+	brlo int_adc_dec
+	reti
+
+int_adc_inc:
+	sbi flag_int, 3
+	reti
+
+int_adc_dec:
+	sbi flag_int, 3
+	reti
+
+int_timer:
+	clr flag_int
+	sbi flag_int, 4
+	reti
