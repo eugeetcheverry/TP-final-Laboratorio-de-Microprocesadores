25733327a4a2f81e49e900a2714ba9e7460fd386
María Eugenia Etcheverry <114704607+eugeetcheverry@users.noreply.github.com>
2024-06-29 11:06:12 -0300

    Add files via upload

--- leds_titilando.asm
+++ leds_titilando.asm
@@ -2,6 +2,8 @@
 
 
  .def aux_SREG = r22
+ .def vleds = r23
+ .def rleds = r24
  .org SRAM_START
 
 .cseg
@@ -26,11 +28,33 @@ main:
 	ldi r16, 0b00001111
 	out ddrb, r16 
 
-	;Defino un contador
-	clr r17 ; para ver si se llego al top
-	ldi r18, 6 ; para ver si se iero 6 veces
+	rcall leds_titilando
 
-inicializo_timer:
+wait: 
+	rjmp wait
+
+/*leds_titilando
+	ldi r16, 0b10000000
+	out TCCR1A, r16
+	ldi r16, 0x0f
+	out OCR1AH, r16
+	ldi r16, 0b00111100
+	ldi OCRIAL, r16
+	ldi r16, 0x02
+	sts TIMSK1, r16
+	sei ; Habilito las interrupciones globales
+	ldi rleds, 6
+	ldi vleds, 0x0f
+	ldi r16, 0b00001101
+	sts TCCR1B, r16; cuando seteamos el prescaler 64 arranca a contar
+
+
+toggle_leds: 
+	out PINC, vleds 
+	out PINB, vleds ; haciendo con toggle
+	reti*/
+
+leds_titilando:
 	cli
 	ldi r16, 0b10000000
 	sts TCCR1A, r16
@@ -38,31 +62,31 @@ inicializo_timer:
 	sts OCR1AH, r16
 	ldi r16, 0b00111100 ;para una frecuencia de 2hz
 	sts OCR1AL, r16
-	ldi r16, 0b00000010
+	clr r16
 	sts TIMSK1, r16 ; TIFR1 1
 	sei
+	ldi rleds, 6
+	ldi vleds, 0b00001111
 	ldi r16, 0b00001101
 	sts TCCR1B, r16
-
-wait: 
-	rjmp wait
-
 toggle_leds:
-	sbic TIFR1, 1
-	dec r18
-	cpi r18, 0
+	sbis TIFR1, 1
+	rjmp toggle_leds
+	dec rleds
+	cpi rleds, 0
 	breq fin_timer
-	ldi r20, 0b00001111
-	out PINC, r20 
-	out PINB, r20 ; haciendo con toggle
-	reti
+	out PINC, vleds 
+	out PINB, vleds 
+	sbi TIFR1, 1
+	rjmp toggle_leds; haciendo con toggle
 fin_timer:
 	clr r16
-	sts tccr1b, r16
+	sts TCCR1B, r16
 	out PORTC, r16
 	out PORTB, r16
-fin_inter:
-	reti
+	ret
+
+
 
 
 
